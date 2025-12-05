d2b84216293531534f66f7128fd052e0c92188b4
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-12-05 17:50:04 +0600

    Fix syntax error in print statement and ensure proper output for squares and evens

--- Array/list_comprehensive.py
+++ Array/list_comprehensive.py
@@ -1,6 +1,16 @@
 squares = [x**2 for x in range(1,10)]
-print(squares = [x**2 for x in range(1,10)]
-)
+print(squares)
 
 evens =  [x for x in range(1,21) if x % 2 == 0]
 print(evens)
+
+name = ["robeen", "alice", "bob", "charlie"]
+uppercase_names = [n.upper() for n in name]
+print(uppercase_names)
+
+matrix = [[i*j for j in range(1,4)] for i in range(1,4)]
+print(matrix)
+
+numbers = [1, 2, 3, 4, 5]
+result = ["Even " if x % 2 == 0 else "Odd" for x in numbers]
+print(result)
